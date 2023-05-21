ecf279317887b5f422734613389ef746e8ce865d
Enital <117684114+Enital@users.noreply.github.com>
2023-05-21 16:59:34 +0300

    friend

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -3,10 +3,11 @@ import PropTypes from "prop-types";
 
 export const FriendListItem = ({ friends }) => {
     return (
-        friends.map(({avatar,name,isOnline,id}) => {
+        friends.map(({ avatar, name, isOnline, id }) => {
+            const statusColor = (isOnline) ? 'green' : 'red';
             return (
                 <li className="friend-item" key={id}>
-                <span className="friend-status">{ isOnline }</span>
+                <span className="friend-status" style={{backgroundColor: statusColor}}></span>
                 <img className="friend-avatar" src={ avatar } alt="User avatar" width="48" />
                 <p className="friend-name">{ name }</p>
         </li>
